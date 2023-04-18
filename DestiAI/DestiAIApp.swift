2c825d955ca55064e499c1d712ac31432dcd3fb4
Lorand Ignat <lorand.ignat@nagarro.com>
2023-04-18 11:12:31 +0300

    Initial Commit

---
+++ DestiAI/DestiAIApp.swift
@@ -0,0 +1,17 @@
+//
+//  DestiAIApp.swift
+//  DestiAI
+//
+//  Created by Lorand Ignat on 18.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct DestiAIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
